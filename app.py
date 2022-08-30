acb2e9f9d1c75d05e393f12cd9302c9e368c3258
Hugo Fabris <hugomfabris@gmail.com>
2022-08-30 12:26:15 -0300

    Fouth commit - Chessdotcom class added to App.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from chessdotcom import get_player_game_archives
 import requests
-import json
 
-#This function will get all games played by some passed username and write then in a new file, in pgn format (chess notation)
-def get_all_games_pgn(username):
-    data = get_player_game_archives(username).json
-    #This first url give us access to a list, in which every index represents information about games played ina  month
-    url = data['archives']
-    #The first for loop will itarate over the months, the second over the games in each month
-    for i in range(len(url)):
-        games = requests.get(url[i]).json()
-        for j in range(len(games['games'])):
-            with open('games.pgn', 'a') as file:
-                file.write(f"{games['games'][j]['pgn']}")
+class Chessdotcom():
+    def __init__(self, username):
+        self._username = username
+    
+    def get_achieves(self):
+        data = get_player_game_archives(self._username).json
+        #This url give us access to a list, in which every index represents information about games played in a certain month, from less to most recent
+        url = data['archives']
+        return url
 
+    @property
+    def get_all_games(self):
+        url = self._username.get_achieves()
+        #The first for loop will itarate over the months, the second over the games in each month
+        for i in range(len(url)):
+            games = requests.get(url[i]).json()
+            for j in range(len(games['games'])):
+                with open('games.pgn', 'a') as file:
+                    file.write(f"{games['games'][j]['pgn']}")
 
-get_all_games_pgn('ilvolpe')
+ilvolpe = Chessdotcom('ilvolpe')
+print(ilvolpe.get_achieves())
